696d0a88d5afe3a92676ae43b767adc4f2a3b6f1
raincityhack <42696972+raincityhack@users.noreply.github.com>
2022-01-17 15:44:10 +0700

    Create index.php

---
+++ repo/ue4dumper/index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+  <title>Rain City Gaming Tech</title>
+  <body>
+    <head>
+       <div>
+        <h2>Welcome...</h2>
+      </div>
+    </head>
+  </body>
+</html>
